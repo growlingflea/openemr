b53ba94f4b911fefbb784b3edaab5b9566f31cd0
daniel <daniel@mi-squared.com>
2014-09-12 11:01:11 -0700

    Usability improvement.  CHanged the title of the Encounter drop down box to \'Encounter Menu\' from \'Encounter History\'

--- interface/main/left_nav.php
+++ interface/main/left_nav.php
@@ -796,7 +796,7 @@ function clearactive() {
  //This function writes the drop down in the top frame.
  //It is called when a new patient is create/selected from the search menu.
   var str = '<Select class="text" id="EncounterHistory" onchange="{top.restoreSession();toencounter(this.options[this.selectedIndex].value)}">';
-  str+='<option value=""><?php echo htmlspecialchars( xl('Encounter History'), ENT_QUOTES) ?></option>';
+  str+='<option name=""><?php echo htmlspecialchars( xl('Encounter Menu'), ENT_QUOTES) ?></option>';
   str+='<option value="New Encounter"><?php echo htmlspecialchars( xl('New Encounter'), ENT_QUOTES) ?></option>';
   str+='<option value="Past Encounter List"><?php echo htmlspecialchars( xl('Past Encounter List'), ENT_QUOTES) ?></option>';
   for(CountEncounter=0;CountEncounter<EncounterDateArray.length;CountEncounter++)
